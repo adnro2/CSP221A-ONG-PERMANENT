d913813880e274175bcfa31c659e34d485e56f57
Andrew Ong <ong.josephandrew@gmail.com>
2026-08-17 21:50:26 +0800

    Add robot tasks and battery exceptyion

--- robot_fleet.py
+++ robot_fleet.py
@@ -1,17 +1,24 @@
-from abc import ABC
-from abc import abstractmethod
+from abc import ABC, abstractmethod
+
+
+class InsufficientBatteryError(Exception):
+    """Raised when a robot attempts an action without sufficient battery."""
+
+    def __init__(self, robot_name, available_battery, required_battery):
+        message = f"Robot '{robot_name}' has insufficient battery. Available: {available_battery}%, Required: {required_battery}%."
+        super().__init__(message)
+        self.robot_name = robot_name
+        self._battery = available_battery
+        self.requiredBattery = required_battery
 
 class Robot(ABC):
     manufacturer = "Golden Dragon"
     population = 0
 
-    @abstractmethod
-    def perform_task(self): pass
-
     def __init__(self, name, battery=100):
         self.name = name
         self.battery = battery
-        RobotFleet.population += 1
+        Robot.population += 1
 
     @property
     def battery(self):
@@ -21,12 +28,44 @@ class Robot(ABC):
     def battery(self, value):
         if value < 0:
             value = 0
-        if value > 100:
+        elif value > 100:
             value = 100
         self._battery = value
+
+    def use_battery(self, amount):
+        if self.battery >= amount:
+            self.battery -= amount
+        else:
+            raise InsufficientBatteryError(self.name, self.battery, amount)
+
+    @abstractmethod
+    def perform_task(self):
+        pass
 
     def __str__(self):
         return f"{self.name} ({self.battery}% battery)"
 
     def __repr__(self):
-        return f"RobotFleet(name={self.name!r}, battery={self.battery!r})"
+        return f"{self.__class__.__name__}(name={self.name!r}, battery={self.battery!r})"
+
+
+class CleaningRobot(Robot):
+    def __init__(self, name, battery=100, dust_capacity=100):
+        super().__init__(name, battery)
+        self.dust_capacity = dust_capacity
+
+    def perform_task(self):
+        cost = 50
+        self.use_battery(cost)
+        return f"{self.name} successfully vacuumed the area (Capacity: {self.dust_capacity}L)."
+
+
+class DroneRobot(Robot):
+    def __init__(self, name, battery=100, max_altitude=100):
+        super().__init__(name, battery)
+        self.max_altitude = max_altitude
+
+    def perform_task(self):
+        cost = 70
+        self.use_battery(cost)
+        return f"{self.name} flew! Reached {self.max_altitude}m."
